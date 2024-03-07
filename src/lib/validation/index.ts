097fe62aa7e7013aa14de66eba25245b9487ba66
JohnnyProg <jangryta9@gmail.com>
2024-03-07 14:40:28 +0100

    (chore:appwrite): Configured appwrite connection

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,5 +1,8 @@
 import * as z from "zod";
 
 export const SignupValidation = z.object({
-    username: z.string().min(2).max(50),
+    name: z.string().min(2, {message: 'To short'}),
+    username: z.string().min(2, {message: 'To short'}),
+    email: z.string().email({message: 'Invalid email'}),
+    password: z.string().min(8, {message: 'Password must be at least 8 characters long'}),
   });
