1511d942eb8cdebdc10874f8df1d80d4574b77f1
JohnnyProg <jangryta9@gmail.com>
2024-03-07 20:27:06 +0100

    (feat:root): Created bottombar

--- src/components/shared/BottomBar.tsx
+++ src/components/shared/BottomBar.tsx
@@ -1,8 +1,43 @@
+import { bottombarLinks } from '@/constants';
+import { INavLink } from '@/types';
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 
 const BottomBar = () => {
   return (
-    <div>BottomBar</div>
+    <section className='bottom-bar'>
+      {bottombarLinks.map((link: INavLink) => {
+            return (
+                <NavLink
+                  key={link.label}
+                  to={link.route}
+                  className={({ isActive }) =>
+                    [
+                      "flex-center flex-col gap-1 p-2 transition",
+                      isActive ? "bg-primary-500 rounded-[10px]" : "",
+                    ].join(" ")
+                  }
+                >
+                  {({ isActive }) => (
+                    <>
+                      <img
+                        src={link.imgURL}
+                        alt={link.label}
+                        width={16}
+                        height={16}
+                        className={
+                          isActive ? "invert-white" : "group-hover:invert-white"
+                        }
+                      />
+                      <p className='tiny0-edium text-light-2'>{link.label}</p>
+                    </>
+                  )}
+                </NavLink>
+
+            );
+          })}
+
+    </section>
   )
 }
 
