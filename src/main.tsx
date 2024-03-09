d5561fd86727bea09182ca4b3e513c29a0354a43
JohnnyProg <jangryta9@gmail.com>
2024-03-09 20:05:49 +0100

    (chore: deploy): Changed BrowserRouter to HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import ReactDOM from "react-dom/client";
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App";
 import AuthProvider from "./context/AuthContext";
 import { QueryProvider } from "./lib/react-query/QueryProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
 		<QueryProvider>
 			<AuthProvider>
 				<App />
 			</AuthProvider>
 		</QueryProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
