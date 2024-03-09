fdca61e2f020c9fd957fc7e0ebfe8859f528f9a0
JohnnyProg <jangryta9@gmail.com>
2024-03-09 20:33:34 +0100

    (chore: deploy): Changed vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: '/Instagram2/',
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
