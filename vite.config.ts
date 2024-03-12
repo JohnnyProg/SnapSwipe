c917d9674df892cdeb358d311c6e34427e0f6d04
JohnnyProg <jangryta9@gmail.com>
2024-03-12 20:01:39 +0100

    (chore:eployment): Changed name of website

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Instagram2/',
+  base: '/SnapSwipe/',
   plugins: [react()],
   resolve: {
     alias: {
